5188c325f292e3d4167da9c0e8f11ae35ad4e820
MuhRizAkbar <2410817210016@mhs.ulm.ac.id>
2024-10-21 21:17:14 +0800

    Update PRAK105-2410817210016-MUHAMMADRIZKYAKBAR.py

--- modul-1/Soal5/PRAK105-2410817210016-MUHAMMADRIZKYAKBAR.py
+++ modul-1/Soal5/PRAK105-2410817210016-MUHAMMADRIZKYAKBAR.py
@@ -3,9 +3,9 @@ bilB = 5
 bilX = 8
 bilY = 8
 
-print("Variabel a bernilai ", bilA);
-print("Variabel b bernilai ", bilB);
-print("Variabel x bernilai ", bilX);
-print("Variabel y bernilai ", bilY);
+print("Variabel a bernilai ", bilA)
+print("Variabel b bernilai ", bilB)
+print("Variabel x bernilai ", bilX)
+print("Variabel y bernilai ", bilY)
 print("Total sisa bagi dari a dibagi b dan x dibagi y adalah ",(bilA%bilB)+(bilX%bilY))
     
